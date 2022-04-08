dc3014139d9dfe987df8408f619ceedb3e7aefb5
githubmilan94 <milankuilya100@gmail.com>
2022-04-08 16:20:40 +0530

    employee list

---
+++ src/com/company/Employee.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employee {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Milan");
+        list.add("kuilya");
+        list.add("One");
+        list.add("two");
+        System.out.println(list.size());
+        System.out.println(list);
+    }
+}

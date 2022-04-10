2a67293502fac70f62f2557640d46f5dad4fda78
githubmilan94 <milankuilya100@gmail.com>
2022-04-10 11:16:21 +0530

    string commit

---
+++ src/org/milan/Stringg.java
@@ -0,0 +1,8 @@
+package org.milan;
+
+public class Stringg {
+    public static void main(String[] args) {
+        System.out.println("Milan" + 1+2);
+        System.out.println(1+3 + "Milan");
+    }
+}

ad184be48d82b419c1ec49f68038b3017915b592
githubmilan94 <milankuilya100@gmail.com>
2022-04-13 10:25:45 +0530

    linked list commit

---
+++ src/org/milan/LinkList.java
@@ -0,0 +1,19 @@
+package org.milan;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkList {
+    public static void main(String[] args) {
+        List<Integer> ll = new LinkedList<>();
+        ll.add(1);
+        ll.add(2);
+        ll.add(3);
+        ll.add(4);
+        ll.add(5);
+        ll.add(6);
+        System.out.println(ll.size());
+        System.out.println(ll);
+
+    }
+}

cc83e41d0bcd2d82c859bac2d83b8d4c5ff36668
githubmilan94 <milankuilya100@gmail.com>
2022-04-13 23:20:51 +0530

    print all pairs of sum

---
+++ src/org/milan/printAllPairs.java
@@ -0,0 +1,22 @@
+package org.milan;
+
+public class printAllPairs {
+
+    public static void printAllPairsSum(int [] arr,int n, int sum){
+
+        for (int i = 0; i <n;i++){
+            for (int j = i + 1; j<n;j++){
+                if(arr[i] + arr[j] == sum){
+                    System.out.println("(" + arr[i] + ", " + arr[j] + ")");
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {1, 5, 7, -1, 5 };
+        int n = arr.length;
+        int sum = 6;
+        printAllPairsSum(arr,n,sum);
+    }
+}

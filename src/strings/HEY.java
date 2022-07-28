13eb8356e4993c33d51f25f51925490d7bd1742e
githubmilan94 <milankuilya100@gmail.com>
2022-07-28 18:59:09 +0530

    string to int covert commit

---
+++ src/strings/HEY.java
@@ -0,0 +1,12 @@
+package strings;
+
+public class HEY {
+    public static void main(String[] args) {
+        String s1 = "100";
+        String s2 = "200";
+        Integer i1 = Integer.parseInt(s1);
+        Integer i2 = Integer.parseInt(s2);
+        Integer i3 = i1 * i2;
+        System.out.println(i3);
+    }
+}
